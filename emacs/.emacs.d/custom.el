4e187e29e7be29c9408bca1142bb81b72b32b5a8
Oscar Duignan <oscarduignan@gmail.com>
2016-02-16 20:08:31 +0000

    Update browse-url browser to google-chrome-stable

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,6 +7,8 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-generic-program "google-chrome-stable")
  '(custom-safe-themes
    (quote
     ("4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" "12b4427ae6e0eef8b870b450e59e75122d5080016a9061c9696959e50d578057" default)))
