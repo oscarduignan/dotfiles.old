d6a9fc22f6794afda13bb4d048a6538cc91e4e36
Oscar Duignan <oscarduignan@gmail.com>
2016-02-17 10:54:57 +0000

    Update neotree setup
    Using the minor mode config I found at
    https://github.com/andrewmcveigh/emacs.d/blob/4d8431b6f6f2b2661eec5014c0ff31ab70f80ede/lisp/init-neotree.el
    that makes neotree behave more like NERDtree which seems to work well
    for me and I now more understand use-package's :init and :config!

--- emacs/.emacs.d/custom/setup-neotree.el
+++ emacs/.emacs.d/custom/setup-neotree.el
@@ -1,14 +1,59 @@
+(defun neotree-copy-file ()
+  (interactive)
+  (let* ((current-path (neo-buffer--get-filename-current-line))
+         (msg (format "Copy [%s] to: "
+                      (neo-path--file-short-name current-path)))
+         (to-path (read-file-name msg (file-name-directory current-path))))
+    (dired-copy-file current-path to-path t))
+  (neo-buffer--refresh t))
+
 (use-package neotree
+  :commands neotree-projectile-action
   :bind ([f8] . neotree-toggle)
+  :init
+  (setq projectile-switch-project-action 'neotree-projectile-action)
   :config
-  (progn
-    ;(setq projectile-switch-project-action 'neotree-projectile-action)
-    (add-hook 'neotree-mode-hook
-      (lambda ()
-	(define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-	(define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-	(define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-	(define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)
-	(define-key evil-normal-state-local-map (kbd "RET") 'neotree-change-root)))))
+  ;; https://github.com/andrewmcveigh/emacs.d/blob/4d8431b6f6f2b2661eec5014c0ff31ab70f80ede/lisp/init-neotree.el
+  (define-minor-mode neotree-evil
+  "Use NERDTree bindings on neotree."
+  :lighter " NT"
+  :keymap (progn
+	    (evil-make-overriding-map neotree-mode-map 'normal t)
+	    (evil-define-key 'normal neotree-mode-map
+	    "C" 'neotree-change-root
+	    "U" 'neotree-select-up-node
+	    "r" 'neotree-refresh
+	    "o" 'neotree-enter
+	    (kbd "<return>") 'neotree-enter
+	    "H" 'neotree-hidden-file-toggle
+	    "q" 'neotree-hide
+	    "i" 'neotree-enter-horizontal-split
+	    "s" 'neotree-enter-vertical-split
+	    "n" 'evil-search-next
+	    "N" 'evil-search-previous
+	    "ma" 'neotree-create-node
+	    "mc" 'neotree-copy-file
+	    "md" 'neotree-delete-node
+	    "mm" 'neotree-rename-node
+	    "gg" 'evil-goto-first-line
+	    "gi" (lambda ()
+		    (interactive)
+		    (if (string= pe/get-directory-tree-external-command
+				nt/gitignore-files-cmd)
+			(progn (setq pe/get-directory-tree-external-command
+				    nt/all-files-cmd))
+		    (progn (setq pe/get-directory-tree-external-command
+				    nt/gitignore-files-cmd)))
+		    (nt/refresh))
+	    "I" (lambda ()
+		    (interactive)
+		    (if pe/omit-enabled
+			(progn (setq pe/directory-tree-function
+				    'pe/get-directory-tree-async)
+			    (pe/toggle-omit nil))
+		    (progn (setq pe/directory-tree-function
+				'pe/get-directory-tree-external)
+			    (pe/toggle-omit t)))))
+	    neotree-mode-map)))
 
 (provide 'setup-neotree)
